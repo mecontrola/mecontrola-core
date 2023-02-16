4b644c9814ea5cd313ccb9311162eba18c24bbca
Rafael Almeida de Oliveira <mecontrola@gmail.com>
2023-02-16 10:14:12 -0300

    MCC-3 Ajuste no Builder para simplificar e adicionado testes.

---
+++ MeControla.Core.Tests/Builders/BaseBuilderTests.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using MeControla.Core.Tests.Mocks;
+using MeControla.Core.Tests.Mocks.Builders;
+using Xunit;
+
+namespace MeControla.Core.Tests.Builders
+{
+    public class BaseBuilderTests
+    {
+        [Fact(DisplayName = "[BaseBuilder.ToBuild] Deve criar um objeto do tipo criado e preenchido com as informações definidas.")]
+        public void DeveCriarUserComCampoNomePreenchido()
+        {
+            var actual = UserBuilder.GetInstance()
+                                    .SetName(DataMock.NAME_DEV_1)
+                                    .ToBuild();
+
+            actual.Uuid.Should().NotBeEmpty();
+            actual.Name.Should().Be(DataMock.NAME_DEV_1);
+        }
+    }
+}
---
+++ MeControla.Core.Tests/Mocks/Builders/UserBuilder.cs
@@ -0,0 +1,15 @@
+﻿using MeControla.Core.Builders;
+using MeControla.Core.Tests.Mocks.Datas.Entities;
+using System;
+
+namespace MeControla.Core.Tests.Mocks.Builders
+{
+    public class UserBuilder : BaseBuilder<UserBuilder, User>, IBuilder<User>
+    {
+        protected override void FillDefaultValues(User obj)
+            => obj.Uuid = Guid.NewGuid();
+
+        public UserBuilder SetName(string value)
+            => Set(obj => obj.Name = value);
+    }
+}
