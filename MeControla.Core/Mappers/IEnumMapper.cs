8dd4c1e702770bf9620110d372bf756c01fb656b
Rafael Almeida de Oliveira <mecontrola@gmail.com>
2024-02-21 10:58:53 -0300

    MCC-10 Alteção no Mapper para atender enums, atualização de bibliotecas.

---
+++ MeControla.Core/Mappers/IEnumMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MeControla.Core.Mappers
+{
+    public interface IEnumMapper<TParam, TResult>
+        where TParam : Enum
+        where TResult : class
+    {
+        TResult ToMap(TParam obj);
+        TResult ToMap(TParam obj, TResult result);
+        IList<TResult> ToMapList<T>(T list)
+            where T : IList<TParam>, ICollection<TParam>;
+    }
+}
