d7136a2e0e8c02db63dd21121d19651d75c8f388
Rafael Almeida de Oliveira <mecontrola@gmail.com>
2024-02-22 16:38:08 -0300

    MCC-11 Adicionar ajustes para o automapper trabalhar com a conversão de Enum para Class.

---
+++ MeControla.Core/Mappers/BaseEnumToDtoMapper.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using MeControla.Core.Data.Enums;
+using MeControla.Core.Extensions;
+using System;
+
+namespace MeControla.Core.Mappers
+{
+#if !DEBUG
+        [System.Diagnostics.DebuggerStepThrough]
+#endif
+    public abstract class BaseEnumToDtoMapper<TParam, TResult> : InternalBaseMapper<TParam, TResult>, IEnumToDtoMapper<TParam, TResult>
+         where TParam : struct, Enum
+         where TResult : BaseEnumItemDto, new()
+    {
+        protected override void MapFields(IMappingExpression<TParam, TResult> map)
+            => map.ConvertUsing(source => new()
+            {
+                Id = Convert.ToUInt32(source),
+                Value = source.GetDescription()
+            });
+    }
+}
