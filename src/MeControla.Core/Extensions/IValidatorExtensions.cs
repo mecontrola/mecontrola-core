9267257d300b1c2559107604a4165403c1edacbf
Rafael Almeida de Oliveira <mecontrola@gmail.com>
2024-07-05 09:44:33 -0300

    MCC-17 Adicionado extensão para o Ivalidator e gerar exceção quando o InputDto for inválido.

---
+++ src/MeControla.Core/Extensions/IValidatorExtensions.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using MeControla.Core.Data.Dtos;
+using MeControla.Core.Data.Entities;
+using MeControla.Core.Exceptions;
+using System;
+
+namespace MeControla.Core.Extensions
+{
+#if !DEBUG
+    [System.Diagnostics.CodeAnalysis.DoesNotReturn]
+#endif
+    public static class IValidatorExtensions
+    {
+#if !DEBUG
+        [System.Diagnostics.DebuggerStepThrough]
+        [System.Diagnostics.CodeAnalysis.DoesNotReturn]
+#endif
+        public static void ThrowIfInvalid<TEntity, TInputDto>(this IValidator<TInputDto> validator, TInputDto input)
+            where TEntity : class, IEntity
+            where TInputDto : class, IInputDto
+        {
+            ArgumentNullException.ThrowIfNull(validator);
+
+            var result = validator.Validate(input);
+            if (result.IsValid)
+                return;
+
+            throw new FormValidationException(typeof(TEntity), result);
+        }
+    }
+}
---
+++ test/MeControla.Core.Tests/Mocks/Validators/UserInputDtoValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using MeControla.Core.Tests.Mocks.Datas.InputDtos;
+
+namespace MeControla.Core.Tests.Mocks.Validators
+{
+    public class UserInputDtoValidator : AbstractValidator<UserInputDto>, IValidator<UserInputDto>
+    {
+        public const string FIELD_NAME_REQUIRED = "Name is required.";
+        public const string FIELD_NAME_BETWEENLENGTH = "Name should be between 5 and 100 chars";
+
+        public UserInputDtoValidator()
+        {
+            RuleFor(x => x.Name).Cascade(CascadeMode.Stop)
+                                .NotEmpty()
+                                .WithMessage(FIELD_NAME_REQUIRED)
+                                .Must(x => x.Length > 5 && x.Length < 100)
+                                .WithMessage(FIELD_NAME_BETWEENLENGTH);
+        }
+    }
+}
