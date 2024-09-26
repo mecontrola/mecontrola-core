d64a0ee2ba2225ab612b74fc2d36b5472eb1372b
Rafael Almeida de Oliveira <mecontrola@gmail.com>
2024-09-26 17:29:09 -0300

    MCC-21 Adicionacio uma extensão para Expression, permite juntar dois predicates com AND, com seus testes.

---
+++ src/MeControla.Core/Extensions/ExpressionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace MeControla.Core.Extensions;
+
+/// <summary>
+/// Provides extension methods for working with Expression.
+/// </summary>
+public static class ExpressionExtensions
+{
+    /// <summary>
+    /// Combines two expressions into one using the logical AND operator.
+    /// </summary>
+    /// <typeparam name="T">The type of the data.</typeparam>
+    /// <param name="source">The source expression.</param>
+    /// <param name="target">The target expression.</param>
+    /// <returns>A combined expression representing the logical AND of the two expressions.</returns>
+    public static Expression<Func<T, bool>> Combine<T>(this Expression<Func<T, bool>> source, Expression<Func<T, bool>> target)
+    {
+        var parameter = Expression.Parameter(typeof(T));
+
+        return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(Expression.Invoke(source, parameter), Expression.Invoke(target, parameter)), parameter);
+    }
+}
